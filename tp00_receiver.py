5eaf4a28d713ba81a54cb0d64f83a2f3ef1fa8f5
Emely Da Mata <56152001+emelydamata@users.noreply.github.com>
2021-03-08 23:23:51 +0000

    Update tp00_receiver.py

--- tp00_receiver.py
+++ tp00_receiver.py
@@ -1,55 +1,52 @@
-import os
-import socket
 import getpass
-from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
+import socket
 from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
+from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
 from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives import hashes, hmac
 
-''' Construção da Classe Receiver '''
 
-
-# var p/ uso na def execute.
-lim = 2048
 
 class Receiver:
+
 
     def __init__(con):
         host = 'localhost'
         port = 8282
         con.conn = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         # Se necessário a reutilização do endereço/porta:
-        con.conn.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR,3)
+        con.conn.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         print("*** Aguardando uma Conexão com Emitter ***")
         con.address = (host, port)
 
-    ''' Criando uma Conexão. '''
+    '''conectando  '''
 
     def conexao(con):
         con.conn.bind(con.address)
         con.conn.listen(1)
 
-    '''Decifrar a Mensagem'''
+    ''' decifrando a mensagem '''
 
-    def decipher(con,key, iv, tag, msg):
+    def decipher(con, key, iv, tag, msg):
         cipher = Cipher(algorithms.AES(key), modes.GCM(iv, tag), default_backend())
         decifrar = cipher.decryptor()
         text_limpo = decifrar.update(msg) + decifrar.finalize()
         return text_limpo
 
-    ''' Principal: '''
+    ''' Fun principal'''
 
     def execute(con):
+        lim = 10000
         con.conexao()
         i = 0
         while (i < 1):
-            print('Aguardando Mensagem...')
-            connec, emitter = con.conn.accept()
-            print("Conectado com: ", emitter)
+            adrr, emitter = con.conn.accept()
+            print("Conectado por: ", emitter)
             i += 1
             password = requestPassWord()
+
             while True:
-                msg = connec.recv(lim)
+                msg = adrr.recv(lim)
                 if (not msg):
                     break
                 else:
@@ -58,32 +55,34 @@ class Receiver:
                     iv = msg[48:64]
                     tag = msg[64:80]
                     ciphertext = msg[80:]
+
                     key = derivation(salt).derive(password)
-                try:
-                    texto_limpo = con.decipher(key, iv, tag, ciphertext)
-                    if (t == authenticationMac(funcaoHash(key), texto_limpo)):
-                        print(texto_limpo.decode())
-                    else:
-                        print('Está Mensagem não Possui Chave Autenticada.')
-                except:
-                    print('Mensagem Corrompida!\n')
-                    con.finish(connec)
-            con.finish(connec)
+
+                    try:
+                        text_limpo = con.decipher(key, iv, tag, ciphertext)
+                        if (t == mac(Hash(key), text_limpo)):
+                            print(text_limpo.decode())
+                        else:
+                            print('Mensagem comprometida. Chave não autenticada')
+                    except:
+                        print('Mensagem comprometida\n')
+                        con.finish(con)
+            con.finish(con)
         return
 
-    '''Função Para Encerrar a Conexão: '''
-
-    def finish(con,connec):
-        con.conn.close()
+    def finish(con, connec):
+        connec.close()
         print(' Encerrando Conexão...')
         return
 
 
-''' Tratativa de Password e a Derivação da Key: '''
+''' Tratativa de Password '''
 
 def requestPassWord():
     password = getpass.getpass('Insira a Password: ')
     return str.encode(password)
+
+''' Derivação da Key:'''
 
 def derivation(salt):
     kdf = PBKDF2HMAC(algorithm=hashes.SHA256(),
@@ -93,20 +92,22 @@ def derivation(salt):
                      backend=default_backend())
     return kdf
 
-''' Autenticação de Hash da Mensagem'''
-def authenticationMac(key, source, tag):
+''' Autenticação '''
+
+def mac(key, source, tag):
     hmac_ = hmac.HMAC(key, hashes.SHA256(), default_backend())
     hmac_.update(source)
     if tag == None:
         return hmac_.finalize()
     hmac_.verify(tag)
 
-def funcaoHash(s):
+''' Hash da Mensagem'''
+def Hash(s):
     digest = hashes.Hash(hashes.SHA256(), backend=default_backend())
     digest.update(s)
     return digest.finalize()
 
-
+''' Para atender as fun criadas '''
 
 def chamada():
     rec = Receiver()
